4d07c6277eb9ad030adc5f0208565cce7574c45c
Uvais Ahmad <uvss21@gmail.com>
2024-10-12 15:18:58 +0530

    [Builder ] Extra reducer using Builder

--- src/redux/reducers/notificationReducer.js
+++ src/redux/reducers/notificationReducer.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit"
+import { resetCounter } from "./counterReducer";
 
 const initialState = {
     message: "",
@@ -10,12 +11,21 @@ const notificationSlice = createSlice({
     initialState,
     reducers: {
     },
-    extraReducers: {
-        // The is the actionType of another slice action.
-        "counter/resetCounter": (state) => {
+    // extraReducers: {
+    //     // The is the actionType of another slice action.
+    //     "counter/resetCounter": (state) => {
+    //         state.message = "Counter reset";
+    //         state.type = "info";
+    //     }
+    // }
+
+    // When ExtraReducers defined as an object it receives a builder object.
+    // Using Builder Callbacks
+    extraReducers: (builder) => {
+        builder.addCase(resetCounter, (state, action ) => {
             state.message = "Counter reset";
             state.type = "info";
-        }
+        })
     }
 });
 
